c00253d379c2a2fbaaf64b9427c9f0935f49b2cb
Mikhail Titov <mlt@gmx.us>
2013-11-14 12:28:58 -0600

    i18n for messageboxes

---
+++ i18n/siam_en.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>hypsometry</name>
+    <message>
+        <location filename="hypsometrydialog.py" line="203"/>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="hypsometrydialog.py" line="190"/>
+        <source>abort?</source>
+        <translation type="unfinished">Do you want to shut process pool down?</translation>
+    </message>
+    <message>
+        <location filename="hypsometrydialog.py" line="203"/>
+        <source>close?</source>
+        <translation>Do you want to close this dialog and continue in a background? You&apos;d have to kill pythonw yourself if you want to abort operation? Logging if any won&apos;t update anymore if you proceed.</translation>
+    </message>
+</context>
+</TS>
